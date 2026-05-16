828e1824504562449ef3a75a9f693ac58a634ef4
Noah <justforgmini@gmail.com>
2026-05-16 10:34:12 +1000

    Initial Commit

---
+++ Amaka/AmakaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmakaApp.swift
+//  Amaka
+//
+//  Created by Noah Zipor on 16/5/2026.
+//
+
+import SwiftUI
+
+@main
+struct AmakaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
